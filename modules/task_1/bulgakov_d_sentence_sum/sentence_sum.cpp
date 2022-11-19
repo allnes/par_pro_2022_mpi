96c3c0e22e8802179177f425c058a27e827ddd82
danielbulgakov <bulgach09@gmail.com>
2022-11-19 16:51:59 +0300

    rework

--- modules/task_1/bulgakov_d_sentence_sum/sentence_sum.cpp
+++ modules/task_1/bulgakov_d_sentence_sum/sentence_sum.cpp
@@ -8,116 +8,127 @@
 
 #include <iostream>
 
-// #define DEBUG
+// #define debug
 
-std::string getRandomString(int len) {
-    std::string res = "";
-    static const char chars[] =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-    std::random_device dev;
-    std::mt19937 gen(dev());
-    for (int i = 0; i < len; i++) { res += chars[gen() % (sizeof(chars) - 1)]; }
-    return res;
-}
+// Find line split indexes for each proc
+// split index shouldn`t be on ?!. chars cause of re-addition to cases
+// "!?" "?!" "..." and so on.
+//  leftborder < line <= rightborder
+std::vector<int> parseTextToIndexes(std::string str, int proc_num) {
+    std::vector<int> vectorInd;
+    int splitSize = ceil(str.length() / static_cast<float>(proc_num));
+    int splitCount = proc_num;
+    const int minSplit = 5;
 
-std::string getRandomText(int word_count) {
-    std::string text = "";
-    int str_end_prob = 5;
-    static const std::string str_split[3] = {"?", "!", "."};
+    vectorInd.reserve(proc_num + 1);
+    vectorInd.push_back(0);
 
-    std::random_device dev;
-    std::mt19937 gen(dev());
-
-
-    bool is_last_ended = false;
-    int pr;
-    for (int i = 0; i < word_count; i++) {
-        pr = gen() % str_end_prob;
-
-        if (i == 0) {text += getRandomString(gen() % 10 + 2); continue;}
-        if (i == word_count - 2) {text += " " + getRandomString(gen() % 10 + 2); continue;}
-        if (i == word_count - 1) {text += str_split[gen() % 3]; break;}
-
-
-        if (pr == str_end_prob - 1 && !is_last_ended) {
-            text += str_split[gen() % 3];
-            is_last_ended = true;
-        } else {
-            text += " " + getRandomString(gen() % 10 + 2);
-            is_last_ended = false;
-        }
+    if (static_cast<int>(str.length()) < minSplit) {
+        vectorInd.push_back(str.length());
+        for (int i = 1; i < proc_num; i++)
+            vectorInd.push_back(-1);
+        return vectorInd;
     }
-    return text;
+    if (splitSize <= minSplit) {
+        splitSize = minSplit;
+        splitCount = str.length() / minSplit;
+    }
+
+    for (int i = splitSize; i < static_cast<int>(str.length()) &&
+            static_cast<int>(vectorInd.size()) < splitCount; i += splitSize) {
+        int currind = i;
+
+        while (currind < static_cast<int>(str.length()) && currind < i + splitSize &&
+            (str[currind] == '?' || str[currind] == '!' || str[currind] == '.') ) {
+            currind++;
+        }
+
+        vectorInd.push_back(currind);
+    }
+    vectorInd.push_back(str.length());
+    for (int i = vectorInd.size(); i < static_cast<int>(vectorInd.capacity()); i++) {
+        vectorInd.push_back(-1);
+    }
+
+    return vectorInd;
 }
 
+// isDelimStarted is boolean value that means that we are
+// at the end of line and the following special characters before
+// space or alphabetic symbol contains in
+// delimChatCases are a part of that ending line
+int computeSenteceCount(std::string str) {
+    int cnt = 0;
+    std::string delimCharCases = "?!.";
+    bool isDelimStarted = false;
 
-int getSequentialOperations(std::string str) {
-    int count = 0;
-    std::string str_end = ".?!";
-    #ifdef DEBUG
-    std::string found;
-    #endif
     for (int i = 0; i < static_cast<int>(str.length()); i++) {
-        if (str_end.find(str[i]) == '.' ||
-        str_end.find(str[i]) == '?'||
-        str_end.find(str[i]) == '!') {
-            count++;
-            #ifdef DEBUG
-            found += str[i];
-            #endif
+        if (delimCharCases.find(str[i]) < delimCharCases.length()) {
+            isDelimStarted = true;
+        } else {
+            if (isDelimStarted) {
+                isDelimStarted = false;
+                cnt++;
+            }
         }
     }
-    #ifdef DEBUG
-    std::cout << "FOUND :" << found << std::endl;
-    #endif
-    return count;
+
+    if (isDelimStarted) cnt++;
+
+    return cnt;
 }
 
-int getParallelOperations(std::string global_str, int str_len) {
+int parallelSentenceCount(std::string str) {
     int size, rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const int delta = str_len / size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    std::vector<int> vectorIndeces;
     if (rank == 0) {
-        #ifdef DEBUG
-        std::cout << "DELTA = " << delta << std::endl;
-        std::cout << "TEXT_ALL = " << global_str << std::endl;
-        std::cout << "RANK : " << 0 << "TEXT " << global_str.substr(0, delta) << std::endl;
-        #endif
-        for (int proc = 1; proc < size - 1; proc++) {
-            MPI_Send(global_str.substr(proc * delta).c_str(), delta, MPI_CHAR, proc, 0 , MPI_COMM_WORLD);
-            #ifdef DEBUG
-            std::cout << "RANK : " << proc << "TEXT " << global_str.substr(proc * delta, delta) << std::endl;
-            #endif
-        }
-        if (size > 1) {
-            MPI_Send(global_str.substr((size - 1) * delta).c_str(),
-                delta + str_len % size, MPI_CHAR, size - 1, 0 , MPI_COMM_WORLD);
-            #ifdef DEBUG
-            std::cout << "RANK : " << (size - 1) << "TEXT " <<
-                global_str.substr((size - 1) * delta, delta + str_len % size) << std::endl;
-            #endif
+        vectorIndeces = parseTextToIndexes(str, size);
+        for (int proc = 1; proc < size; proc++) {
+            if (vectorIndeces[proc + 1] == -1) {
+                // In case there were more proccesors than str.length()
+                // or each proccees should compute less than 10 chars
+                // programm wont use some of them.
+                MPI_Send(std::string("NOT USED").c_str(), 9, MPI_CHAR, proc, 0, MPI_COMM_WORLD);
+            } else {
+                int meslen = vectorIndeces[proc + 1] - vectorIndeces[proc];
+                if (meslen == 0) meslen = 1;
+                std::string mes = str.substr(vectorIndeces[proc], meslen);
+
+                #ifdef debug
+                std::cout  << " SEND rank " << proc << " " << mes << std::endl;
+                #endif
+
+                MPI_Send(mes.c_str(), meslen, MPI_CHAR, proc, 0, MPI_COMM_WORLD);
+            }
         }
     }
-
     std::string local_str;
+
     if (rank == 0) {
-        local_str = global_str.substr(0, delta);
+        local_str = str.substr(0, vectorIndeces[1]);
     } else {
         MPI_Status status;
-        // std::cout << "HERE" << std::endl;
-        char* buff = new char[delta + 10];
-        MPI_Recv(buff, delta + 10, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+        int maxbuffsize = (str.length() / size) * 2 + 1 >= 25 ? (str.length() / size) * 2 + 1 : 25;
+        char * buff = new char[maxbuffsize];
+        MPI_Recv(buff, maxbuffsize, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
         local_str = std::string(buff);
     }
-
+    #ifdef debug
+    std::cout  << "GET rank " << rank << " " << local_str << std::endl;
+    #endif
     int global_sum = 0;
-    int local_sum = getSequentialOperations(local_str);
+    int local_sum = computeSenteceCount(local_str);
+
+    #ifdef debug
+    if (local_sum > 0) std::cout << "RANKK" << rank << std::endl;
+    #endif
+
     MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
     return global_sum;
 }
-
 
 
--- modules/task_1/bulgakov_d_sentence_sum/sentence_sum.h
+++ modules/task_1/bulgakov_d_sentence_sum/sentence_sum.h
@@ -3,14 +3,13 @@
 #ifndef MODULES_TASK_1_BULGAKOV_D_SENTENCE_SUM_SENTENCE_SUM_H_
 #define MODULES_TASK_1_BULGAKOV_D_SENTENCE_SUM_SENTENCE_SUM_H_
 
-#include "string"
+#include <string>
+#include <vector>
 
-std::string genRandomString(int len);
+std::vector<int> parseTextToIndexes(std::string str, int proc_num);
 
-std::string getRandomText(int word_count);
+int computeSenteceCount(std::string str);
 
-int getParallelOperations(std::string global_str, int str_len);
-
-int getSequentialOperations(std::string str);
+int parallelSentenceCount(std::string str);
 
 #endif  // MODULES_TASK_1_BULGAKOV_D_SENTENCE_SUM_SENTENCE_SUM_H_
