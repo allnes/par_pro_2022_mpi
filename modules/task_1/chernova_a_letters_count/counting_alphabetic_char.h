0b61b63cb93998bb03c5e32566be2347bfd42bbb
anna-chernova <an.chernova02@gmail.ru>
2022-12-17 10:48:44 +0300

    fix

--- modules/task_1/chernova_a_letters_count/counting_alphabetic_char.h
+++ modules/task_1/chernova_a_letters_count/counting_alphabetic_char.h
@@ -1,15 +1,15 @@
 // Copyright 2022
 #pragma once
-#include <vector>
-#include <string>
 #include <mpi.h>
-#include <random>
-#include <algorithm>
-#include <numeric>
-#include <iostream>
-#include <ctime>
-#include <climits>
 
-std::string getRandomString(size_t  size);
+#include <algorithm>
+#include <climits>
+#include <ctime>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <string>
+#include <vector>
+std::string getRandomString(size_t size);
 int CountingAlphabeticCharParallel(const std::string& str);
 int CountingAlphabeticCharSequential(const std::string& str);
