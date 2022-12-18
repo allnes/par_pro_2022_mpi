691d555e29c0075c22fd203ae28cadb25730c787
anna-chernova <an.chernova02@gmail.ru>
2022-12-19 00:29:25 +0300

    fix

--- modules/task_1/chernova_a_letters_count/ops_mpi.h
+++ modules/task_1/chernova_a_letters_count/ops_mpi.h
@@ -1,13 +1,9 @@
 // Copyright 2018 Nesterov Alexander
-#ifndef MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
-#define MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
-
-#include <vector>
+#pragma once
 #include <string>
+#include <vector>
 
-std::vector<int> getRandomVector(int  sz);
-int getParallelOperations(std::vector<int> global_vec,
-                          int count_size_vector, const std::string& ops);
+std::vector<int> getRandomVector(int sz);
+int getParallelOperations(std::vector<int> global_vec, int count_size_vector,
+                          const std::string& ops);
 int getSequentialOperations(std::vector<int> vec, const std::string& ops);
-
-#endif  // MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
