82030b4584a3e4db5ccb580ed87b9539e6471301
anna-chernova <an.chernova02@gmail.ru>
2022-12-19 00:39:34 +0300

    fix

--- modules/task_1/chernova_a_letters_count/ops_mpi.h
+++ modules/task_1/chernova_a_letters_count/ops_mpi.h
@@ -10,4 +10,4 @@ int getParallelOperations(std::vector<int> global_vec, int count_size_vector,
                           const std::string& ops);
 int getSequentialOperations(std::vector<int> vec, const std::string& ops);
 
-#endif // MODULES_TASK_1_CHERNOVA_A_LETTERS_COUNT_H_
+#endif  // MODULES_TASK_1_CHERNOVA_A_LETTERS_COUNT_H_
