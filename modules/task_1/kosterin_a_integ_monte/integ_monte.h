c12297b7d4638e169e79aa61e2a714c825e24614
korgi <igrok12005@yandex.ru>
2022-12-21 18:34:43 +0300

    first task 0.3

---
+++ modules/task_1/kosterin_a_integ_monte/integ_monte.h
@@ -0,0 +1,6 @@
+// Copyright 2018 Nesterov Alexander
+#ifndef MODULES_TASK_1_KOSTERIN_A_INTEG_MONTE_INTEG_MONTE__H_
+#define MODULES_TASK_1_KOSTERIN_A_INTEG_MONTE_INTEG_MONTE__H_
+double monteCarlo(int low, int high, int count, double (*f)(double));
+
+#endif // MODULES_TASK_1_KOSTERIN_A_INTEG_MONTE_INTEG_MONTE__H_
