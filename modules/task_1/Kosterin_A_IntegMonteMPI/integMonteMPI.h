6ac316e3a6014a106490151f9b345bb97081e507
korgi12005 <74495864+korgi12005@users.noreply.github.com>
2022-12-21 21:50:53 +0300

    Delete modules/task_1/Kosterin_A_IntegMonteMPI directory

--- modules/task_1/Kosterin_A_IntegMonteMPI/integMonteMPI.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2018 Nesterov Alexander
-
-#ifndef MODULES_TASK_1_KOSTERIN_A_INTEGMONTEMPI_INTEGMONTEMPI_H_
-#define MODULES_TASK_1_KOSTERIN_A_INTEGMONTEMPI_INTEGMONTEMPI_H_
-
-#include <random>
-
-double monteCarlo(int low, int high, int count, double (*f)(double));
-#endif // MODULES_TASK_1_KOSTERIN_A_INTEGMONTEMPI_INTEGMONTEMPI_H_
