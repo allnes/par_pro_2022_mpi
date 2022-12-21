c2671b6b0c2b08d2eea27cfe4017800459c07465
anna-chernova <an.chernova02@gmail.ru>
2022-12-21 15:14:14 +0300

    style fix

--- modules/task_1/chernova_a_counting_alphabetic_char/counting_alphabetic_char.h
+++ modules/task_1/chernova_a_counting_alphabetic_char/counting_alphabetic_char.h
@@ -1,11 +1,11 @@
-// Copyright 2018 Nesterov Alexander
+// Copyright 2022 Chernova Anna
 #ifndef MODULES_TASK_1_CHERNOVA_A_COUNTING_ALPHABETIC_CHAR_COUNTING_ALPHABETIC_CHAR_H_
 #define MODULES_TASK_1_CHERNOVA_A_COUNTING_ALPHABETIC_CHAR_COUNTING_ALPHABETIC_CHAR_H_
 
-#include <vector>
 #include <string>
+#include <vector>
 
-std::string getRandomString(size_t  size);
+std::string getRandomString(size_t size);
 int CountingAlphabeticCharParallel(const std::string& str);
 int CountingAlphabeticCharSequential(const std::string& str);
 
--- modules/task_1/chernova_a_counting_alphabetic_char/main.cpp
+++ modules/task_1/chernova_a_counting_alphabetic_char/main.cpp
@@ -1,111 +1,112 @@
-// Copyright 2018 Nesterov Alexander
+// Copyright 2022 Chernova Anna
 #include <gtest/gtest.h>
-#include <vector>
-#include "./counting_alphabetic_char.h"
+
 #include <gtest-mpi-listener.hpp>
+#include <vector>
+
+#include "./counting_alphabetic_char.h"
 
 TEST(Parallel_Operations_MPI, Test_Empty_string) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::string global_vec;
-    const int count_size_vector = 0;
-    if (rank == 0) {
-        global_vec = getRandomString(count_size_vector);
-    }
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::string global_vec;
+  const int count_size_vector = 0;
+  if (rank == 0) {
+    global_vec = getRandomString(count_size_vector);
+  }
 
-    int global_sum = CountingAlphabeticCharParallel(global_vec);
+  int global_sum = CountingAlphabeticCharParallel(global_vec);
 
-    if (rank == 0) {
-        int reference_sum = CountingAlphabeticCharSequential(global_vec);
-        ASSERT_EQ(reference_sum, global_sum);
-    }
+  if (rank == 0) {
+    int reference_sum = CountingAlphabeticCharSequential(global_vec);
+    ASSERT_EQ(reference_sum, global_sum);
+  }
 }
 
 TEST(Parallel_Operations_MPI, Test_Diff) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::string global_vec;
-    const int count_size_vector = 120;
-    int global_diff;
-    if (rank == 0) {
-        global_vec = getRandomString(count_size_vector);
-    }
-    global_diff = CountingAlphabeticCharParallel(global_vec);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::string global_vec;
+  const int count_size_vector = 120;
+  int global_diff;
+  if (rank == 0) {
+    global_vec = getRandomString(count_size_vector);
+  }
+  global_diff = CountingAlphabeticCharParallel(global_vec);
 
-    if (rank == 0) {
-        int reference_diff = CountingAlphabeticCharSequential(global_vec);
-        ASSERT_EQ(reference_diff, global_diff);
-    }
+  if (rank == 0) {
+    int reference_diff = CountingAlphabeticCharSequential(global_vec);
+    ASSERT_EQ(reference_diff, global_diff);
+  }
 }
 
 TEST(Parallel_Operations_MPI, Test_Diff_2) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::string global_vec;
-    const int count_size_vector = 120;
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::string global_vec;
+  const int count_size_vector = 120;
 
-    if (rank == 0) {
-        global_vec = getRandomString(count_size_vector);
-    }
+  if (rank == 0) {
+    global_vec = getRandomString(count_size_vector);
+  }
 
-    int global_diff = CountingAlphabeticCharParallel(global_vec);
+  int global_diff = CountingAlphabeticCharParallel(global_vec);
 
-    if (rank == 0) {
-        int reference_diff = CountingAlphabeticCharSequential(global_vec);
-        ASSERT_EQ(reference_diff, global_diff);
-    }
+  if (rank == 0) {
+    int reference_diff = CountingAlphabeticCharSequential(global_vec);
+    ASSERT_EQ(reference_diff, global_diff);
+  }
 }
 
 TEST(Parallel_Operations_MPI, Test_Max) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::string global_vec;
-    const int count_size_vector = 120;
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::string global_vec;
+  const int count_size_vector = 120;
 
-    if (rank == 0) {
-        global_vec = getRandomString(count_size_vector);
-    }
+  if (rank == 0) {
+    global_vec = getRandomString(count_size_vector);
+  }
 
-    int global_max;
-    global_max = CountingAlphabeticCharParallel(global_vec);
+  int global_max;
+  global_max = CountingAlphabeticCharParallel(global_vec);
 
-    if (rank == 0) {
-        int reference_max = CountingAlphabeticCharSequential(global_vec);
-        ASSERT_EQ(reference_max, global_max);
-    }
+  if (rank == 0) {
+    int reference_max = CountingAlphabeticCharSequential(global_vec);
+    ASSERT_EQ(reference_max, global_max);
+  }
 }
 
 TEST(Parallel_Operations_MPI, Test_Max_2) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::string global_vec;
-    const int count_size_vector = 120;
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::string global_vec;
+  const int count_size_vector = 120;
 
-    if (rank == 0) {
-        global_vec = getRandomString(count_size_vector);
-    }
+  if (rank == 0) {
+    global_vec = getRandomString(count_size_vector);
+  }
 
-    int global_max;
-    global_max = CountingAlphabeticCharParallel(global_vec);
+  int global_max;
+  global_max = CountingAlphabeticCharParallel(global_vec);
 
-    if (rank == 0) {
-        int reference_max = CountingAlphabeticCharSequential(global_vec);
-        ASSERT_EQ(reference_max, global_max);
-    }
+  if (rank == 0) {
+    int reference_max = CountingAlphabeticCharSequential(global_vec);
+    ASSERT_EQ(reference_max, global_max);
+  }
 }
 
-
 int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    MPI_Init(&argc, &argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  MPI_Init(&argc, &argv);
 
-    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-    ::testing::TestEventListeners& listeners =
-        ::testing::UnitTest::GetInstance()->listeners();
+  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+  ::testing::TestEventListeners& listeners =
+      ::testing::UnitTest::GetInstance()->listeners();
 
-    listeners.Release(listeners.default_result_printer());
-    listeners.Release(listeners.default_xml_generator());
+  listeners.Release(listeners.default_result_printer());
+  listeners.Release(listeners.default_xml_generator());
 
-    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-    return RUN_ALL_TESTS();
+  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+  return RUN_ALL_TESTS();
 }
