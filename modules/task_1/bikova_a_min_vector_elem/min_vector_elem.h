a2fc8d51615dfd6b6a1995e5f02e3efe73fadc99
qooju <84714244+qooju@users.noreply.github.com>
2022-12-24 16:03:59 +0300

    Delete min_vector_elem.h

--- modules/task_1/bikova_a_min_vector_elem/min_vector_elem.h
+++
@@ -1,7 +0,0 @@
-#ifndef MIN_VEC_ELEM
-#define MIN_VEC_ELEM
-#include<vector>
-int Smallest_Element(int ProcRank, int ProcSize);
-void Proccess_Initialization(std::vector<int>& vec,int& n,std::vector<int>& ProcVec,int ProcRank, int ProcSize,int& NumElem);
-void DataDistribution(std::vector<int> &vec,int n,std::vector<int>& ProcVec, int ProcRank, int ProcSize,int NumElem);
-#endif
