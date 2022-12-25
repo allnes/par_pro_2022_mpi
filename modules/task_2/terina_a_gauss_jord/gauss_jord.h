6c9483b0c452efb8409d9349be2eb74fe935e70c
imterinaa <84046726+imterinaa@users.noreply.github.com>
2022-12-25 16:00:01 +0300

    second

--- modules/task_2/terina_a_gauss_jord/gauss_jord.h
+++ modules/task_2/terina_a_gauss_jord/gauss_jord.h
@@ -2,7 +2,9 @@
 #include <string>
 #include <vector>
 
-#pragma once
+#ifndef MODULES_TASK_2_TERINA_A_GAUSS_JORD_GAUSS_JORD_H_
+#define MODULES_TASK_2_TERINA_A_GAUSS_JORD_GAUSS_JORD_H_
+
 std::vector<std::vector<double>> getRandomMatrix(std::vector<double> vec,
     int matrix_size);
 int getParallelOperations(std::vector<int> global_vec, int count_size_vector,
@@ -14,3 +16,5 @@ void carve_up(int matrix_size, int part, double* str, double* subtrahend);
 std::vector<double> ParGJ(std::vector<double> str, int matrix_size);
 int evaluateforScatterv(int num_parts, int part_size, int it);
 std::vector<double> matrix_random(std::vector<double> num_vec, int matrix_size);
+
+#endif  //MODULES_TASK_2_TERINA_A_GAUSS_JORD_GAUSS_JORD_H_
