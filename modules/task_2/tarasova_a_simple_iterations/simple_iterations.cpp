15b5a56f39615e1dc714b9b3617d56a5831b0834
taranast <taranast434@gmail.com>
2022-12-27 09:53:25 +0300

    Update simple_iterations.cpp

--- modules/task_2/tarasova_a_simple_iterations/simple_iterations.cpp
+++ modules/task_2/tarasova_a_simple_iterations/simple_iterations.cpp
@@ -33,10 +33,10 @@ std::vector<double> GetSimpleIter(const std::vector<std::vector<double>>& a, con
             b.at(i) /= divid;
         for (int j = 0; j < size; j++) {
             if (i != j) {
-                if (divid < 0)
-                    a.at(i).at(j) /= divid;
-                else { a.at(i).at(j) /= -divid; }
-            } else { a.at(i).at(j) = 0; }
+                a.at(i).at(j) /= (divid < 0) ? divid : -divid;
+            } else { 
+                a.at(i).at(j) = 0; 
+            }
         }
     }
     std::vector<double> pansw = b;
@@ -102,10 +102,10 @@ std::vector<double> GetSimpleIterParallel(const std::vector<std::vector<double>>
             Procdata.at(BIndex) /= divid;
         for (int j = i; j < i + Size - 1; j++) {
             if (j != Index + i) {
-                if (divid < 0)
-                    Procdata.at(j) /= divid;
-                else { Procdata.at(j) /= -divid;}
-            } else { Procdata.at(j) = 0; }
+                Procdata.at(j) /= (divid < 0) ? divid : -divid;
+            } else { 
+                Procdata.at(j) = 0; 
+            }
         }
         Index++;
     }
