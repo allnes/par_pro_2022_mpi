e8955cbc15a6fee5f547850955b2db5d27173bb9
Лев Поляков <lev-polyackov@mail.ru>
2023-01-06 21:37:54 +0300

    polyackov_l_alg_fox

--- modules/task_3/polyackov_l_alg_fox/alg_fox.cpp
+++ modules/task_3/polyackov_l_alg_fox/alg_fox.cpp
@@ -91,7 +91,7 @@ std::vector<double> getResOfFoxMult(std::vector<double> first_matr, std::vector<
     // Data processing
     int block_size, blocks_in_row, new_matr_size;
     if (rank == 0) {
-        block_size = static_cast<int>ceil(sqrt(double(matr_size * matr_size) / size));
+        block_size = static_cast<int>ceil(sqrt(static_cast<double>(matr_size * matr_size) / size));
         blocks_in_row = static_cast<int>round(sqrt(size));
 
         int new_matr_size = block_size * blocks_in_row;
