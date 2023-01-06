4414790e00020d7626b8f4954c64162c3cd94b14
m4karovms <mishamakarov2002@inbox.ru>
2023-01-06 19:33:17 +0300

    task3 commit

---
+++ modules/task_3/makarov_m_sparse_matrix_col/sparse_matrix_col.h
@@ -0,0 +1,27 @@
+// Copyright 2022 Makarov Mikhail
+#ifndef MODULES_TASK_3_MAKAROV_M_SPARSE_MATRIX_COL_SPARSE_MATRIX_COL_H_
+#define MODULES_TASK_3_MAKAROV_M_SPARSE_MATRIX_COL_SPARSE_MATRIX_COL_H_
+
+#include <iostream>
+#include <vector>
+
+struct ComprColStorage {
+  int countRow;
+  int countCol;
+  std::vector<double> value;
+  std::vector<int> startPos;
+  std::vector<int> numberRow;
+
+  bool operator==(const ComprColStorage &sec) const;
+  bool operator!=(const ComprColStorage &sec) const;
+};
+
+ComprColStorage GenerateMatrix(int countRow, int countCol, double proc);
+ComprColStorage GetSequentialSolution(ComprColStorage *first,
+                                      ComprColStorage *second);
+ComprColStorage GetParalSolution(ComprColStorage *first,
+                                 ComprColStorage *second);
+double Sum(const ComprColStorage &first, const ComprColStorage &second,
+           int curRow, int curCol, int size);
+void PrintMatr(const ComprColStorage &matr);
+#endif  // MODULES_TASK_3_MAKAROV_M_SPARSE_MATRIX_COL_SPARSE_MATRIX_COL_H_
